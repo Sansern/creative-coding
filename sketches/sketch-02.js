cbb5c7db4ad1be4056a4450c63b89987a33b3bf6
MintSW <mintnimal@gmail.com>
2021-09-07 02:00:29 +0700

    custom the arcs

--- sketches/sketch-02.js
+++ sketches/sketch-02.js
@@ -19,13 +19,13 @@ const sketch = () => {
     const cx = width * 0.5;
     const cy = height * 0.5;
 
-    const w = width * 0.01;
-    const h = height * 0.1;
+    const w = width * 0.005;
+    const h = height * 0.05;
 
     let x,y;
 
-    const num = 40;
-    const radius = width * 0.3;
+    const num = 35;
+    const radius = width * 0.25;
     
     for (let i = 0; i < num; i++) {
       const slice = math.degToRad(360 / num);
@@ -37,7 +37,7 @@ const sketch = () => {
       context.save();
       context.translate (x,y);
       context.rotate(-angle);
-      context.scale(random.range(1, 3), 1);
+      context.scale(random.range(0.2, 1), 2);
 
       context.beginPath();
       context.rect(-w * 0.5, random.range(0, -h * 0.5), w, h);
@@ -49,10 +49,10 @@ const sketch = () => {
       context.rotate(-angle);
 
 
-      context.lineWidth = random.range(1, 10);
+      context.lineWidth = random.range(0.5, random.range(0,30));
 
       context.beginPath();
-      context.arc(0, 0, radius * random.range(0.2, 1.3), slice * random.range(0, -5), slice * random.range(1, -8));
+      context.arc(4, 0, radius * random.range(0.5, 1.5), slice * random.range(0, -2), slice * random.range(1, -10));
       context.stroke();
       context.restore();
 
